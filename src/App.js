66660db195de51c9d5994a7c0bef6ca2249252ea
Handaru9 <120361562+Handaru9@users.noreply.github.com>
2023-04-13 15:41:00 +0700

    projekapi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ApiPage from './Api';
+
+function App() {
+  return (
+    <div className="App">
+      <ApiPage></ApiPage>
+    </div>
+  );
+}
+
+export default App;
